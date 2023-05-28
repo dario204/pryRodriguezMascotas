f939013bb9f4db3d1813df15a855262bd6fd925e
dario.rod551@gmail.com <dario.rod551@gmail.com>
2023-05-28 16:23:37 -0300

    28-05-23

--- frmMain.Designer.cs
+++ frmMain.Designer.cs
@@ -42,15 +42,15 @@
             this.btnJugar = new System.Windows.Forms.Button();
             this.btnCuidar = new System.Windows.Forms.Button();
             this.btnSalir = new System.Windows.Forms.Button();
-            this.fotoMascota = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.fotoMascota)).BeginInit();
+            this.mcrAgregarMascota = new System.Windows.Forms.GroupBox();
+            this.mcrAgregarMascota.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblVeterinaria
             // 
             this.lblVeterinaria.AutoSize = true;
             this.lblVeterinaria.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblVeterinaria.Location = new System.Drawing.Point(289, 19);
+            this.lblVeterinaria.Location = new System.Drawing.Point(257, 9);
             this.lblVeterinaria.Name = "lblVeterinaria";
             this.lblVeterinaria.Size = new System.Drawing.Size(242, 51);
             this.lblVeterinaria.TabIndex = 0;
@@ -60,7 +60,7 @@
             // 
             this.lblNombre.AutoSize = true;
             this.lblNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNombre.Location = new System.Drawing.Point(39, 114);
+            this.lblNombre.Location = new System.Drawing.Point(12, 42);
             this.lblNombre.Name = "lblNombre";
             this.lblNombre.Size = new System.Drawing.Size(81, 25);
             this.lblNombre.TabIndex = 1;
@@ -70,7 +70,7 @@
             // 
             this.lblEdad.AutoSize = true;
             this.lblEdad.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblEdad.Location = new System.Drawing.Point(39, 183);
+            this.lblEdad.Location = new System.Drawing.Point(12, 111);
             this.lblEdad.Name = "lblEdad";
             this.lblEdad.Size = new System.Drawing.Size(58, 25);
             this.lblEdad.TabIndex = 2;
@@ -80,7 +80,7 @@
             // 
             this.lblTipo.AutoSize = true;
             this.lblTipo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTipo.Location = new System.Drawing.Point(39, 255);
+            this.lblTipo.Location = new System.Drawing.Point(12, 183);
             this.lblTipo.Name = "lblTipo";
             this.lblTipo.Size = new System.Drawing.Size(158, 25);
             this.lblTipo.TabIndex = 3;
@@ -88,14 +88,14 @@
             // 
             // txtNombre
             // 
-            this.txtNombre.Location = new System.Drawing.Point(224, 114);
+            this.txtNombre.Location = new System.Drawing.Point(197, 42);
             this.txtNombre.Name = "txtNombre";
             this.txtNombre.Size = new System.Drawing.Size(178, 22);
             this.txtNombre.TabIndex = 4;
             // 
             // txtEdad
             // 
-            this.txtEdad.Location = new System.Drawing.Point(224, 183);
+            this.txtEdad.Location = new System.Drawing.Point(197, 111);
             this.txtEdad.Name = "txtEdad";
             this.txtEdad.Size = new System.Drawing.Size(178, 22);
             this.txtEdad.TabIndex = 5;
@@ -111,7 +111,7 @@
             "Loro",
             "Pez",
             "Hamster"});
-            this.cboTipo.Location = new System.Drawing.Point(224, 255);
+            this.cboTipo.Location = new System.Drawing.Point(197, 183);
             this.cboTipo.Name = "cboTipo";
             this.cboTipo.Size = new System.Drawing.Size(178, 24);
             this.cboTipo.TabIndex = 6;
@@ -120,16 +120,16 @@
             // 
             this.lstMascotas.FormattingEnabled = true;
             this.lstMascotas.ItemHeight = 16;
-            this.lstMascotas.Location = new System.Drawing.Point(44, 336);
+            this.lstMascotas.Location = new System.Drawing.Point(44, 373);
             this.lstMascotas.Name = "lstMascotas";
-            this.lstMascotas.Size = new System.Drawing.Size(275, 164);
+            this.lstMascotas.Size = new System.Drawing.Size(320, 164);
             this.lstMascotas.TabIndex = 7;
             this.lstMascotas.SelectedIndexChanged += new System.EventHandler(this.lstMascotas_SelectedIndexChanged);
             // 
             // btnAgregar
             // 
             this.btnAgregar.Font = new System.Drawing.Font("Yu Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAgregar.Location = new System.Drawing.Point(546, 156);
+            this.btnAgregar.Location = new System.Drawing.Point(519, 84);
             this.btnAgregar.Name = "btnAgregar";
             this.btnAgregar.Size = new System.Drawing.Size(149, 73);
             this.btnAgregar.TabIndex = 8;
@@ -140,7 +140,7 @@
             // btnAlimentar
             // 
             this.btnAlimentar.Font = new System.Drawing.Font("Yu Gothic", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAlimentar.Location = new System.Drawing.Point(44, 544);
+            this.btnAlimentar.Location = new System.Drawing.Point(44, 556);
             this.btnAlimentar.Name = "btnAlimentar";
             this.btnAlimentar.Size = new System.Drawing.Size(122, 39);
             this.btnAlimentar.TabIndex = 10;
@@ -151,7 +151,7 @@
             // btnJugar
             // 
             this.btnJugar.Font = new System.Drawing.Font("Yu Gothic", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnJugar.Location = new System.Drawing.Point(198, 544);
+            this.btnJugar.Location = new System.Drawing.Point(198, 556);
             this.btnJugar.Name = "btnJugar";
             this.btnJugar.Size = new System.Drawing.Size(121, 39);
             this.btnJugar.TabIndex = 11;
@@ -162,7 +162,7 @@
             // btnCuidar
             // 
             this.btnCuidar.Font = new System.Drawing.Font("Yu Gothic", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCuidar.Location = new System.Drawing.Point(44, 628);
+            this.btnCuidar.Location = new System.Drawing.Point(44, 627);
             this.btnCuidar.Name = "btnCuidar";
             this.btnCuidar.Size = new System.Drawing.Size(122, 39);
             this.btnCuidar.TabIndex = 12;
@@ -173,7 +173,7 @@
             // btnSalir
             // 
             this.btnSalir.Font = new System.Drawing.Font("Yu Gothic", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSalir.Location = new System.Drawing.Point(720, 628);
+            this.btnSalir.Location = new System.Drawing.Point(653, 655);
             this.btnSalir.Name = "btnSalir";
             this.btnSalir.Size = new System.Drawing.Size(110, 39);
             this.btnSalir.TabIndex = 13;
@@ -181,38 +181,39 @@
             this.btnSalir.UseVisualStyleBackColor = true;
             this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
             // 
-            // fotoMascota
+            // mcrAgregarMascota
             // 
-            this.fotoMascota.Image = global::pryRodriguezMascotas.Properties.Resources.pez;
-            this.fotoMascota.Location = new System.Drawing.Point(546, 336);
-            this.fotoMascota.Name = "fotoMascota";
-            this.fotoMascota.Size = new System.Drawing.Size(284, 168);
-            this.fotoMascota.TabIndex = 14;
-            this.fotoMascota.TabStop = false;
+            this.mcrAgregarMascota.Controls.Add(this.txtEdad);
+            this.mcrAgregarMascota.Controls.Add(this.lblNombre);
+            this.mcrAgregarMascota.Controls.Add(this.lblEdad);
+            this.mcrAgregarMascota.Controls.Add(this.lblTipo);
+            this.mcrAgregarMascota.Controls.Add(this.txtNombre);
+            this.mcrAgregarMascota.Controls.Add(this.btnAgregar);
+            this.mcrAgregarMascota.Controls.Add(this.cboTipo);
+            this.mcrAgregarMascota.Location = new System.Drawing.Point(44, 119);
+            this.mcrAgregarMascota.Name = "mcrAgregarMascota";
+            this.mcrAgregarMascota.Size = new System.Drawing.Size(719, 231);
+            this.mcrAgregarMascota.TabIndex = 14;
+            this.mcrAgregarMascota.TabStop = false;
+            this.mcrAgregarMascota.Text = "Agregar Mascota";
             // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(875, 679);
-            this.Controls.Add(this.fotoMascota);
+            this.ClientSize = new System.Drawing.Size(785, 706);
+            this.Controls.Add(this.mcrAgregarMascota);
             this.Controls.Add(this.btnSalir);
             this.Controls.Add(this.btnCuidar);
             this.Controls.Add(this.btnJugar);
             this.Controls.Add(this.btnAlimentar);
-            this.Controls.Add(this.btnAgregar);
             this.Controls.Add(this.lstMascotas);
-            this.Controls.Add(this.cboTipo);
-            this.Controls.Add(this.txtEdad);
-            this.Controls.Add(this.txtNombre);
-            this.Controls.Add(this.lblTipo);
-            this.Controls.Add(this.lblEdad);
-            this.Controls.Add(this.lblNombre);
             this.Controls.Add(this.lblVeterinaria);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "frmMain";
             this.Text = "Inicio";
-            ((System.ComponentModel.ISupportInitialize)(this.fotoMascota)).EndInit();
+            this.mcrAgregarMascota.ResumeLayout(false);
+            this.mcrAgregarMascota.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -233,7 +234,7 @@
         private System.Windows.Forms.Button btnJugar;
         private System.Windows.Forms.Button btnCuidar;
         private System.Windows.Forms.Button btnSalir;
-        private System.Windows.Forms.PictureBox fotoMascota;
+        private System.Windows.Forms.GroupBox mcrAgregarMascota;
     }
 }
 
